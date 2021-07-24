22ca1892741a09ab0a724a0fcd381270f5ba1c6f
Patrik M. Rosenström <patrik.millvik@gmail.com>
2021-07-25 01:54:51 +0200

    Kernel: Moved the printing and serial port code to seperate files
    * Moved the printing code from the main file to 'print.rs'
    * Moved the serial port code from the main file to 'serial.rs'

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(asm, panic_info_message)]
 #![no_std]
 
+mod serial;
+#[macro_use] mod print;
 mod mm;
 mod multiboot;
 
@@ -14,111 +16,6 @@ const KERNEL_TEXT_START: usize = 0xffffffff80000000;
 const KERNEL_TEXT_SIZE: usize = 1 * 1024 * 1024 * 1024;
 const KERNEL_TEXT_END: usize = KERNEL_TEXT_START + KERNEL_TEXT_SIZE - 1;
 
-fn out8(address: u16, data: u8) {
-    unsafe {
-        asm!("out dx, al", in("dx") address, in("al") data);
-    }
-}
-
-fn in8(address: u16) -> u8 {
-    let value: u8;
-    unsafe {
-        asm!("in al, dx", out("al") value, in("dx") address);
-    }
-    value
-}
-
-struct SerialPort {
-    port: u16,
-}
-
-impl SerialPort {
-    fn new(port: u16) -> Self {
-        out8(port + 1, 0x00);    // Disable all interrupts
-        out8(port + 3, 0x80);    // Enable DLAB (set baud rate divisor)
-        out8(port + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
-        out8(port + 1, 0x00);    //                  (hi byte)
-        out8(port + 3, 0x03);    // 8 bits, no parity, one stop bit
-        out8(port + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
-        out8(port + 4, 0x0B);    // IRQs enabled, RTS/DSR set
-        out8(port + 4, 0x0F);
-
-        Self {
-            port
-        }
-    }
-
-    fn is_transmit_empty(&self) -> bool {
-        return in8(self.port + 5) & 0x20 != 0;
-    }
-
-    fn output_char(&mut self, c: char) {
-        while !self.is_transmit_empty() {}
-
-        out8(self.port, c as u8);
-    }
-}
-
-impl core::fmt::Write for SerialPort {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        for c in s.chars() {
-            self.output_char(c);
-        }
-
-        Ok(())
-    }
-}
-
-static SERIAL_PORT: Mutex<Option<SerialPort>> = Mutex::new(None);
-
-#[macro_export]
-macro_rules! print {
-    ($($arg:tt)*) => ($crate::_print_fmt(format_args!($($arg)*)))
-}
-
-// Print macro that appends a newline to the end of a print
-#[macro_export]
-macro_rules! println {
-    () => ($crate::print!("\n"));
-    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)))
-}
-
-#[macro_export]
-macro_rules! eprint {
-    ($($arg:tt)*) => {{
-        ecolor_on();
-        $crate::_print_fmt(format_args!($($arg)*));
-        ecolor_off();
-    }}
-}
-
-// Print macro that appends a newline to the end of a print
-#[macro_export]
-macro_rules! eprintln {
-    () => ($crate::eprint!("\n"));
-    ($($arg:tt)*) => ($crate::eprint!("{}\n", format_args!($($arg)*)))
-}
-
-fn ecolor_on() {
-    print!("\x1b[1;31m");
-}
-
-fn ecolor_off() {
-    print!("\x1b[0m");
-}
-
-fn _print_fmt(args: core::fmt::Arguments) {
-    use core::fmt::Write;
-
-    let mut lock = SERIAL_PORT.lock();
-    match lock.as_mut() {
-        Some(f) => {
-            f.write_fmt(args).unwrap();
-        }
-        None => {
-        }
-    }
-}
 
 struct BootPhysicalMemory;
 
@@ -195,9 +92,7 @@ static BOOT_PHYSICAL_MEMORY: BootPhysicalMemory = BootPhysicalMemory {};
 
 #[no_mangle]
 extern fn kernel_init(multiboot_addr: usize) -> ! {
-    {
-        *SERIAL_PORT.lock() = Some(SerialPort::new(0x3f8));
-    }
+    serial::initialize();
 
     let ptr = 0xb8000 as *mut u16;
     unsafe {
---
+++ kernel/src/print.rs
@@ -0,0 +1,51 @@
+//! This module handles printing to the user
+//! For now we only have one way to show the user infomation
+//! and that is the serial port and that is a good starting point
+//! because QEMU can print the serial port to the terminal but
+//! in the future we want to show the user the infomation on
+//! the display
+
+use crate::serial;
+use spin::Mutex;
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::print::_print_fmt(format_args!($($arg)*)))
+}
+
+// Print macro that appends a newline to the end of a print
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)))
+}
+
+#[macro_export]
+macro_rules! eprint {
+    ($($arg:tt)*) => {{
+        $crate::print::ecolor_on();
+        $crate::print::_print_fmt(format_args!($($arg)*));
+        $crate::print::ecolor_off();
+    }}
+}
+
+// Print macro that appends a newline to the end of a print
+#[macro_export]
+macro_rules! eprintln {
+    () => ($crate::eprint!("\n"));
+    ($($arg:tt)*) => ($crate::eprint!("{}\n", format_args!($($arg)*)))
+}
+
+pub fn ecolor_on() {
+    print!("\x1b[1;31m");
+}
+
+pub fn ecolor_off() {
+    print!("\x1b[0m");
+}
+
+pub fn _print_fmt(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+
+    serial::print_fmt(args);
+}
